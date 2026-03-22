400e53083f2dce28610513fc87f56164db79bf19
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-03-22 14:55:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your code here.
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	const progressFillls = document.querySelectorAll(".progress-fill");
+
+	progressFillls.forEach((fill) => {
+		const width = fill.style.widthl
+		fill.style.width = "0";
+
+		setTimeout(() => {
+			fill.style.width = width;
+		}, 100);
+	})
+})
